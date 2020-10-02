960b758966a21fec1aafbd1b5408affa00e66665
amsmu <amarjeetsingh75@gmail.com>
2020-10-02 07:05:03 +0530

    [Update] - changing the port number to 8000. Conflicting with web.

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.use(function (err, req, res, next) {
   else res.status(500).json({ message: err.message });
 });
 
-app.listen(3000, function () {
-  console.log('Server is listening on port 3000');
+app.listen(8000, function () {
+  console.log('Server is listening on port 8000');
 });
